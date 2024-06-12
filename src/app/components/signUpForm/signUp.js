854db48aa834449a7e82621f6d3ff3c0dd421c4b
Eca Mac <alessandroponza01@gmail.com>
2024-06-12 10:25:16 +0200

    finto style forms, mancante vettore smile per UX

---
+++ src/app/components/signUpForm/signUp.js
@@ -0,0 +1,24 @@
+import InputForm from "../inputForm/inputForm";
+import styles from "./signUp.module.css";
+
+const SignUpForm = () => {
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h3 className="text-2xl">Non hai un account?</h3>
+      <p className="text-base">Crealo subito in pochi passi</p>
+      <form className="flex flex-col gap-4">
+        <InputForm type="text" placeholder="Nome" />
+        <InputForm type="text" placeholder="Cognome" />
+        <InputForm type="email" placeholder="Email per la registrazione" />
+        <InputForm
+          type="password"
+          placeholder="Password per la registrazione"
+        />
+        <InputForm type="password" placeholder="Conferma la password" />
+        <InputForm type="submit" value="Registrati" />
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,14 @@ import Image from "next/image";
 import SignInForm from "./components/signInForm/signInForm";
 
 import styles from "./home.module.css";
+import SignUpForm from "./components/signUpForm/signUp";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center gap-4">
-      <section className={styles.signInSection}>
-        <SignInForm />
-      </section>
-      <hr />
-      <section className={styles.signUpSection}>
-        <h1>Sezione Sign Up</h1>
-        <p>test per vedere se funziona git con il mac</p>
-      </section>
+    <div className="h-screen flex flex-col justify-center items-center gap-4">
+      <SignInForm />
+      <hr className="w-2/5" />
+      <SignUpForm />
     </div>
   );
 }
